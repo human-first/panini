ff2dcc9bf90968a68bcf059b5daf4255c97ffc92
Roberto Ramos <roberto@humanfirst.es>
2022-09-05 13:21:11 +0200

    Init commit

---
+++ panini/lib/loadPartials.js
@@ -0,0 +1,20 @@
+var fs    = require('fs');
+var path  = require('path');
+var utils = require('./utils');
+
+/**
+ * Looks for files with .html, .hbs, or .handlebars extensions within the given directory, and adds them as Handlebars partials matching the name of the file.
+ * @param {string} dir - Folder to check for partials.
+ */
+module.exports = function (dir) {
+  var full_dir_path = (process.cwd() + '/' + dir).replace(/\\/g, '/') + (dir.indexOf('/', dir.length - 1) == -1 ? '/' : ''),
+    partials = utils.loadFiles(dir, '**/*.{html,hbs,handlebars}');
+
+  for (var i in partials) {
+    var ext = path.extname(partials[i]),
+      file = fs.readFileSync(partials[i]),
+      name = partials[i].substring(full_dir_path.length, partials[i].length - ext.length);
+
+    this.Handlebars.registerPartial(name, file.toString() + '\n');
+  }
+}
